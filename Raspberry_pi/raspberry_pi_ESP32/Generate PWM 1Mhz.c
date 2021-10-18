f87e9cf54c66dca76724a2d7b0ffa367d8aa24a4
OlfaCherif <92148964+OlfaCherif@users.noreply.github.com>
2021-10-18 09:37:41 +0100

    Update Generate PWM 1Mhz.c

--- Raspberry_pi/raspberry_pi_ESP32/Generate PWM 1Mhz.c
+++ Raspberry_pi/raspberry_pi_ESP32/Generate PWM 1Mhz.c
@@ -19,24 +19,22 @@ int main(int arglen, char *argv[])
              exit(-1);
       }
 
-    //pwmFrequency in Hz = 19.2e6 Hz / pwmClock / pwmRange
-    int clock= 2;
-    int range = 9;
+   
 
     //These values is chosen to generate 1Mhz of frequency 
     int value = 512;
     // duty cycle is from 0 to 1024
 
     wiringPiSetup();
+    pwmSetMode(mode); // This mode is used to choose a desired frequency
 
     pinMode(pinT1,PWM_OUTPUT); //only PCM_GPIO18 support PWM_OUTPUT
     
-
+ //pwmFrequency in Hz = 19.2e6 Hz / pwmClock / pwmRange
+    int clock= 2;
+    int range = 9;
 
     pwmSetRange(range); //Range could be up to 4096
-
-    pwmSetMode(mode); // This mode is used to choose a desired frequency
-
     pwmSetClock(clock); // clock should be in [2..4095]
 
     for (index= 0;index<10000;index++)
